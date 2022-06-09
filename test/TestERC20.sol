e0fbff391cc12b15bb989d7888c6241309c89cb6
leekt <leekt216@gmail.com>
2022-06-09 10:54:24 +0900

    added more test cases and now uses the cheat.assume instead of return

--- test/TestERC20.sol
+++ test/TestERC20.sol
@@ -7,63 +7,76 @@ import "./ICheatCodes.sol";
 
 contract TestERC20 is SampleERC20, Test {
     CheatCodes cheat = CheatCodes(HEVM_ADDRESS);
-    SampleERC20 ERC20;
+    SampleERC20 token;
     address addr1 = 0x393b996C9456Bc870ACb3dE3F987528C07D0580a;
     address addr2 = 0x458165AdAB6c343ea150C9D0Ae441040Eb3c502f;
 
     constructor(){
-        ERC20 = new SampleERC20();
+        token = new SampleERC20();
     }
 
     function setUp() public {
-        ERC20.mint(addr1, 1e18);
-        ERC20.mint(addr2, 1e18);
+        token.mint(addr1, 1e18);
+        token.mint(addr2, 1e18);
     }
 
     function testTransfer(uint256 value) public {
-        if(value == 0) return;
-        value = value % 1e18;
-        if(ERC20.balanceOf(addr2) >= value) return;
+        cheat.assume(value <= token.balanceOf(addr1));
 
-        uint256 reciverBalance = ERC20.balanceOf(addr2);
-        uint256 senderBalacne = ERC20.balanceOf(addr1);
+        uint256 receiverBalance = token.balanceOf(addr2);
+        uint256 senderBalance = token.balanceOf(addr1);
         
         cheat.prank(addr1);
-        ERC20.transfer(addr2, value);
-        require(senderBalacne - ERC20.balanceOf(addr1) == value, "Balance didn't decrease");
-        require(ERC20.balanceOf(addr2) - reciverBalance == value, "Balance didn't increase");
+        token.transfer(addr2, value);
+
+        assertEq(senderBalance - token.balanceOf(addr1), value);
+        assertEq(token.balanceOf(addr2) - receiverBalance, value);
     }
 
-    function testTransferFrom(uint256 value) public {
-        if(value == 0) return;
-        value = value % 1e18;
-        if(ERC20.balanceOf(addr2) >= value) return;
-        if(ERC20.allowance(addr1, addr2) < value) return;
-
-        uint256 reciverBalance = ERC20.balanceOf(addr2);
-        uint256 senderBalacne = ERC20.balanceOf(addr1);
-
-        ERC20.transferFrom(addr2, addr1, value); // from to value
-        require(senderBalacne - ERC20.balanceOf(addr1) == value, "Balance didn't decrease");
-        require(ERC20.balanceOf(addr2) - reciverBalance == value, "Balance didn't increase");
+    function testFailTransfer(uint256 value) public {
+        cheat.assume(value > token.balanceOf(addr1));
+        cheat.prank(addr1);
+        token.transfer(addr2,value);
     }
 
-    function testApprove(uint256 value, uint256 amount) public {
-        value = value % 1e18;
-        if(value == 0) return;
-        if(value < amount) return;
-        if(ERC20.balanceOf(addr2) < amount) return;
+    function testTransferFrom(uint256 allowance, uint256 transferAmount) public {
+        cheat.assume(allowance >= transferAmount);
+        cheat.assume(allowance < type(uint256).max); // to handle cases where erc20 contract assumes max allowance as infinite allowance
+        cheat.assume(token.balanceOf(addr1) >= transferAmount);
         
         cheat.prank(addr1);
-        ERC20.approve(addr2, value); // approve addr2 to spend addr1's balance
-        require(ERC20.allowance(addr1, addr2) == value, "Allownace didn't change");
-
-        emit log_uint(value);
-        emit log_uint(amount);
+        token.approve(addr2, allowance); // approve addr2 to spend addr1's balance
+        assertEq(token.allowance(addr1, addr2), allowance);
 
         cheat.prank(addr2); // change msg.sender to addr2 (also, cheat.prank only lasts for 1 external call so we need to set prank everytime we call external non-view functions)
-        //ERC20.transferFrom(addr2, addr1, amount);
-        ERC20.transferFrom(addr1, addr2, amount);
-        require(ERC20.allowance(addr1, addr2) == value - amount, "Allowance didn't decrease properly");
+        //token.transferFrom(addr2, addr1, amount);
+        token.transferFrom(addr1, addr2, transferAmount);
+        assertEq(token.allowance(addr1, addr2), allowance - transferAmount);
+    }
+
+    /// @dev tests if transferFrom fails when allowance is low
+    function testFailTransferFromAllowance(uint256 allowance, uint256 transferAmount) public {
+        cheat.assume(transferAmount <= token.balanceOf(addr1));
+        cheat.assume(transferAmount > allowance);
+        cheat.prank(addr1);
+        token.approve(addr2, allowance);
+        cheat.prank(addr2);
+        token.transferFrom(addr1,addr2,transferAmount);
+    }
+
+    /// @dev tests if transferFrom fails when balance is low
+    function testFailTransferFromBalance(uint256 allowance, uint256 transferAmount) public {
+        cheat.assume(transferAmount > token.balanceOf(addr1));
+        cheat.assume(transferAmount <= allowance);
+        cheat.prank(addr1);
+        token.approve(addr2, allowance);
+        cheat.prank(addr2);
+        token.transferFrom(addr1,addr2,transferAmount);
+    }
+
+    function testApprove(uint256 allowance) public {
+        cheat.prank(addr1);
+        token.approve(addr2, allowance);
+        assertEq(token.allowance(addr1, addr2), allowance);
     }
 }
